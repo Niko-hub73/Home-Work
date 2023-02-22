d436cb6e0e75f9f4b2363cad9323d157b39a49e6
Niko-hyb73 <Pushnikov34nikolay@gmail.com>
2023-02-22 22:21:37 +0300

    lesson121 повтор темы числа

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-user1 = 123
+user1 = 1**100
 print(user1)
-1
+user = 2**16
+print(user)
+user = 2-1000
+print(user)
+user = 2
+user1 = 2
+print(user+user1*user)
+user = 60
+user1 = 3
+print(user/user1)
+user = 144
+import math
+
+print(math.sqrt(144))
+
+total1 = 66
+total2 = 11
+x = total1-total2
+print(x)
+a = 8
+b = 16
+c = (b-a)
+x = c*c
+print(x)
+import math
+
+print(math.sqrt(568))
+
+
